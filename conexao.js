3d987c6eb07393c63e515fa287034333450798f2
Carlos Daniel <carlinhosdan31@gmail.com>
2022-08-01 17:01:33 -0300

    mudar configurações do banco

--- conexao.js
+++ conexao.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'nameUser',
-    host: 'localhost',
-    database: 'biblioteca',
-    password: 'yourPassword',
-    port: 5432
+    user: 'agicmdsrtyhfnv',
+    host: 'ec2-50-19-255-190.compute-1.amazonaws.com',
+    database: 'd7g5c1feemi9lu',
+    password: '4d6c3106180f3a50bf4136d167c592ed017938c7f671760ccb949ce231f23774',
+    port: 5432,
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
